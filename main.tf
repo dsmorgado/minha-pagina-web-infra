e936119610e9c44568c605b1d09c6d73bb35f0e5
Diana Morgado <dianamorgado7@gmail.com>
2025-06-03 22:58:56 +0100

    Terraform update2

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-resource "aws_ec2_instance" "minha_instancia" {
+resource "aws_instance" "minha_instancia" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              docker pull dsmorgado/minha-pagina-web:latest
+              docker run -d -p 80:80 dsmorgado/minha-pagina-web:latest
+              EOF
 
   tags = {
     Name = "MinhaPaginaWeb"
--- variables.tf
+++ variables.tf
@@ -1,2 +1,29 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  description = "Chave de acesso AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "Chave secreta AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_image" {
+  description = "Imagem Docker para correr"
+  type        = string
+  default     = "dsmorgado/minha-pagina-web:latest"
+}
+
+variable "instance_type" {
+  description = "Tipo de instância EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "AMI para instância EC2 (Amazon Linux 2)"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"  # us-east-1
+}
